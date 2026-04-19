d1638b908580523c7c7e4cc4007db1c83563f638
Khaja Gesudaraz <gesudarazahmed@example.com>
2026-04-19 13:36:37 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -12,27 +12,16 @@ dotenv.config({
 
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(` server is running at the port:${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed !!! ",err);
+    
+})
 
 
 
